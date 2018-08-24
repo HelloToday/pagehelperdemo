6753f968ea04422b6af614851240fece062b837f
unknown <jinshanbai@08-201507200464.creditease.corp>
2018-08-24 18:33:15 +0800

    测试配置文件和运行环境一致

--- pagehelper/src/test/java/com/bjs/demp/pagehelper/test/EmergencyDirect.java
+++ pagehelper/src/test/java/com/bjs/demp/pagehelper/test/EmergencyDirect.java
@@ -1,29 +1,34 @@
 package com.bjs.demp.pagehelper.test;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
 
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 
-import com.bjs.demo.pagehelper.dao.QueueDao;
+import com.alibaba.fastjson.JSONObject;
+import com.bjs.demo.pagehelper.model.QueueEntity;
 import com.bjs.demo.pagehelper.service.QueueService;
+import com.github.pagehelper.PageHelper;
 
-@ContextConfiguration(locations = { "classpath:spring-mvc.xml", "classpath:spring-mvc.xml" })
+@ContextConfiguration(locations = {  "classpath:spring-config.xml" })
 public class EmergencyDirect  extends AbstractJUnit4SpringContextTests {
 	@Resource
 	private QueueService queueService;
 	@Test
 	public void testPagePlugin() throws Exception{
+		PageHelper.startPage(10000, 10);
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("userId", 298666);
 		map.put("system", 4);
 		map.put("startIndex", 0); 
 		map.put("pageSize", 10);
-		queueService.queryForPageForAgent(map);
+		List<QueueEntity> list = queueService.queryForPageForAgent(map);
+		System.out.println(list.size());
+		System.out.println(JSONObject.toJSONString(list));
 	}
 }
